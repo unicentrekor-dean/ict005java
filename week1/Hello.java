d2f5f6d51b5dc8ccf88ac9e8d59685bf29b52c62
unicentrekor-dean <79134093+unicentrekor-dean@users.noreply.github.com>
2022-09-20 17:36:00 +0900

    Add files via upload

---
+++ week1/Hello.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Java World!!");
+		System.out.println();
+		System.out.println();
+	}
+}
